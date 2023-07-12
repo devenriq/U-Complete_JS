80c13a30de70d6597a5d232510541d56cb9c3d90
enrique palomino <octepc.97@gmail.com>
2023-07-12 17:54:46 -0500

    feat: :sparkles: Made a game in which it's necessary to find a number by clues that tells if it's too much quantity or too less

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,37 +1,95 @@
 "use strict";
 
-let hello = "hello";
-console.log(hello);
+// let hello = "hello";
+// console.log(hello);
 
-const bills = [125, 555, 44];
-const tips = [];
+// const bills = [125, 555, 44];
+// const tips = [];
 
-const calcTip = (billsList) => {
-  for (let i = 0; i < billsList.length; i++) {
-    let temp = 0;
-    billsList[i] > 50 && billsList[i] < 300
-      ? (temp = billsList[i] * 0.15)
-      : (temp = billsList[i] * 0.2);
-    tips.push(temp);
+// const calcTip = (billsList) => {
+//   for (let i = 0; i < billsList.length; i++) {
+//     let temp = 0;
+//     billsList[i] > 50 && billsList[i] < 300
+//       ? (temp = billsList[i] * 0.15)
+//       : (temp = billsList[i] * 0.2);
+//     tips.push(temp);
+//   }
+// };
+
+// calcTip(bills);
+
+// console.log(tips);
+
+// const enrique = {
+//   firstName: "enrique",
+//   lastName: "Furtado",
+//   age: 83,
+//   job: "Pilot",
+//   friends: ["Michael", "Peter", "Susan"],
+// };
+
+// let bestFriend = console.log(
+//   `${enrique.firstName} has ${
+//     enrique.friends.length
+//   } friends and his best friend is ${
+//     enrique.friends[Math.floor(Math.random() * enrique.friends.length)]
+//   }`
+// );
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// const temperatureAmplitude = (listOfTemperatures) => {
+//   const temperaturesNumbers = [];
+//   for (let i = 0; i < listOfTemperatures.length; i++) {
+//     const element = listOfTemperatures[i];
+//     if (typeof element === "number") {
+//       temperaturesNumbers.push(element);
+//     }
+//   }
+//   for (let i = 0; i < temperaturesNumbers.length - 1; i++) {
+//     for (let j = 0; j < temperaturesNumbers.length - 1 - i; j++) {
+//       if (temperaturesNumbers[j] > temperaturesNumbers[j + 1]) {
+//         let temp = temperaturesNumbers[j];
+//         temperaturesNumbers[j] = temperaturesNumbers[j + 1];
+//         temperaturesNumbers[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   const min = temperaturesNumbers[0];
+//   const max = temperaturesNumbers[temperaturesNumbers.length - 1];
+
+//   return max - min;
+// };
+
+// console.log(temperatureAmplitude(temperatures));
+
+// const temperatureAmplitudeAlternative = (array) => {
+//   let listOfValues = [];
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     if (typeof element === "number") {
+//       listOfValues.push(element);
+//     }
+//   }
+
+//   const min = Math.min(...listOfValues);
+//   const max = Math.max(...listOfValues);
+//   return max - min;
+// };
+
+// console.log(temperatureAmplitudeAlternative(temperatures));
+
+const temperatureAmplitudeAlternativeAlternative = (array) => {
+  let minValue = array[0],
+    maxValue = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minValue) minValue = array[i];
+    if (array[i] > maxValue) maxValue = array[i];
   }
+
+  return maxValue - minValue;
 };
 
-calcTip(bills);
-
-console.log(tips);
-
-const enrique = {
-  firstName: "enrique",
-  lastName: "Furtado",
-  age: 83,
-  job: "Pilot",
-  friends: ["Michael", "Peter", "Susan"],
-};
-
-let bestFriend = console.log(
-  `${enrique.firstName} has ${
-    enrique.friends.length
-  } friends and his best friend is ${
-    enrique.friends[Math.floor(Math.random() * enrique.friends.length)]
-  }`
-);
+console.log(temperatureAmplitudeAlternativeAlternative(temperatures));
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const hiddenNumber = document.querySelector('.number');
+const statusMessage = document.querySelector('.message');
+const initialScorePage = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
+const background = document.querySelector('body');
+
+const inputNumber = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const reloadButton = document.querySelector('.again');
+
+const randomNumber = Math.floor(Math.random() * 20);
+let initialScore = 20;
+
+checkButton.addEventListener('click', () => {
+  const guessedNumber = parseFloat(inputNumber.value);
+
+  console.log(guessedNumber);
+
+  if (guessedNumber > randomNumber) {
+    statusMessage.textContent = "🔼 It's more than expected";
+    initialScore--;
+    initialScorePage.textContent = initialScore;
+  } else if (guessedNumber < randomNumber) {
+    statusMessage.textContent = "🔽 It's less than expected";
+    initialScore--;
+    initialScorePage.textContent = initialScore;
+  } else {
+    hiddenNumber.textContent = randomNumber;
+    background.style.backgroundColor = '#60b347';
+    highScore.textContent = initialScore;
+  }
+});
+
+reloadButton.addEventListener('click', () => {
+  location.reload();
+});
+
+console.log(randomNumber);
