d6f1ecdd566475439aa05fe1fc9eb60d942ad8df
enrique palomino <octepc.97@gmail.com>
2023-07-13 11:54:38 -0500

    refactor: :art: Refactored the whole code
    Separated the different actions of the app in different functions to work as pleased

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,40 +1,53 @@
-'use strict';
-
+// Variables
 const hiddenNumber = document.querySelector('.number');
 const statusMessage = document.querySelector('.message');
 const initialScorePage = document.querySelector('.score');
 const highScore = document.querySelector('.highscore');
 const background = document.querySelector('body');
-
 const inputNumber = document.querySelector('.guess');
 const checkButton = document.querySelector('.check');
 const reloadButton = document.querySelector('.again');
-
-const randomNumber = Math.floor(Math.random() * 20);
+let randomNumber = Math.floor(Math.random() * 20);
 let initialScore = 20;
 
-checkButton.addEventListener('click', () => {
+// Funciones
+const updateScore = () => {
+  initialScore--;
+  initialScorePage.textContent = initialScore;
+};
+
+const showMessage = message => {
+  statusMessage.textContent = message;
+};
+
+const showCorrectNumber = () => {
+  hiddenNumber.textContent = randomNumber;
+  background.style.backgroundColor = '#60b347';
+  highScore.textContent = initialScore;
+};
+
+const checkGuess = () => {
   const guessedNumber = parseFloat(inputNumber.value);
 
-  console.log(guessedNumber);
-
   if (guessedNumber > randomNumber) {
-    statusMessage.textContent = "🔼 It's more than expected";
-    initialScore--;
-    initialScorePage.textContent = initialScore;
+    showMessage("⛔️ It's more than expected");
+    updateScore();
   } else if (guessedNumber < randomNumber) {
-    statusMessage.textContent = "🔽 It's less than expected";
-    initialScore--;
-    initialScorePage.textContent = initialScore;
+    showMessage("⛔️ It's less than expected");
+    updateScore();
   } else {
-    hiddenNumber.textContent = randomNumber;
-    background.style.backgroundColor = '#60b347';
-    highScore.textContent = initialScore;
+    showMessage("✅ That's the correct number!");
+    showCorrectNumber();
   }
-});
+};
 
-reloadButton.addEventListener('click', () => {
+const resetGame = () => {
   location.reload();
-});
+};
 
+// Event Listeners
+checkButton.addEventListener('click', checkGuess);
+reloadButton.addEventListener('click', resetGame);
+
+// Código principal
 console.log(randomNumber);
