639455c7a38e61714c3c09e68eb5e93a54fd25d8
enrique palomino <octepc.97@gmail.com>
2023-07-13 12:20:26 -0500

    feat: :sparkles: FInished a little program to show / hide modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Variables
+const buttonsModal = document.querySelectorAll('.show-modal');
+const hiddenElements = document.querySelectorAll('.hidden');
+const buttonClose = document.querySelector('.close-modal');
+
+// Funciones
+const showHiddenElements = () => {
+  hiddenElements.forEach(element => {
+    element.classList.remove('hidden');
+  });
+};
+
+const hideElements = () => {
+  hiddenElements.forEach(element => {
+    element.classList.add('hidden');
+  });
+};
+
+const closeOnEsc = event => {
+  if (event.key === 'Escape') {
+    hideElements();
+  }
+};
+
+// Event Listeners
+buttonsModal.forEach(element => {
+  element.addEventListener('click', showHiddenElements);
+});
+
+buttonClose.addEventListener('click', hideElements);
+
+document.addEventListener('keydown', closeOnEsc);
